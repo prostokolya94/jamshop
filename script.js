d5c4fedb07f084e32c727c0b344ffbc137684f6f
Nikolay Krivosheev <krivosheevnikolay1@yandex.ru>
2020-04-01 20:53:37 +0500

    carousel

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-/* этот код помечает картинки, для удобства разработки */
+const carousel= document.querySelector('.carousel');
 let i = 1;
-for(let li of carousel.querySelectorAll('product-img')) {
-  li.style.position = 'relative';
-  li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
-  i++;
-};
+    for(let li of carousel.querySelectorAll('.product-img')) {
+      li.style.position = 'relative';
+      li.insertAdjacentHTML('beforeend', `<span style="position:absolute;left:0;top:0">${i}</span>`);
+      i++;
+    }
 
-/* конфигурация */
-let width = 169; // ширина картинки
-let count = 3; // видимое количество изображений
+    /* конфигурация */
+    let width = 185; // ширина картинки
+    let count = 1; // видимое количество изображений
 
-let list = carousel.querySelectorAll('ul');
-let listElems = carousel.querySelectorAll('li');
+    let list = carousel;
+    let listElems = carousel.querySelectorAll('.product-img');
+    let position = 0; // положение ленты прокрутки
 
-let position = 0; // положение ленты прокрутки
+    document.querySelector('.prev').onclick = function() {
+      // сдвиг влево
+      position += width * count;
+      // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
+      position = Math.min(position, 0)
+      list.style.marginLeft = position + 'px';
+    };
 
-carousel.querySelector('.arrow-prev').onclick = function() {
-  // сдвиг влево
-  position += width * count;
-  // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
-  position = Math.min(position, 0)
-  list.style.marginLeft = position + 'px';
-};
-
-carousel.querySelector('.arrow-next').onclick = function() {
-  // сдвиг вправо
-  position -= width * count;
-  // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
-  position = Math.max(position, -width * (listElems.length - count));
-  list.style.marginLeft = position + 'px';
-};
+    document.querySelector('.next').onclick = function() {
+      // сдвиг вправо
+      position -= width * count;
+      // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
+      position = Math.max(position, -width * (listElems.length - count));
+      list.style.marginLeft = position + 'px';
+    };
